baf84b5e5604a36480a017f17dc3adcd0a0c394b
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2013-08-09 10:32:56 +0200

    Added segments and cells
    Segments represent 5 cells in a line, such as a section of wall that
    could be shot by a bullet or the inside or outside edge of a tank

---
+++ Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Core/Segment.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndrewTweddle.BattleCity.Core.States;
+using AndrewTweddle.BattleCity.Core.Elements;
+using AndrewTweddle.BattleCity.Core.Collections;
+
+namespace AndrewTweddle.BattleCity.Core
+{
+    /// <summary>
+    /// A segment represents 5 cells in a line.
+    /// This can be used to represent an inside our outside edge of a tank 
+    /// or a segment of a wall (for calculating the effects of a hit on the centre of the wall).
+    /// </summary>
+    public class Segment
+    {
+        public Point Centre;
+        public Axis Axis;
+
+        public IEnumerable<Cell> GetCellsOnSegment(BitMatrix walls, bool includeOutOfBounds = true)
+        {
+            CellState cellState;
+            bool checkLowerBound = true;
+            bool isUpperBoundExceeded = false;
+
+            switch (Axis)
+            {
+                case Core.Axis.Horizontal:
+                    for (short x = (short) (Centre.X - Constants.TANK_EXTENT_OFFSET); x < (short) (Centre.X + Constants.TANK_EXTENT_OFFSET); x++)
+                    {
+                        if (checkLowerBound)
+                        {
+                            if (isUpperBoundExceeded)
+                            {
+                                cellState = CellState.OutOfBounds;
+                            }
+                            else
+                            {
+                                if (x < 0)
+                                {
+                                    cellState = CellState.OutOfBounds;
+                                }
+                                else
+                                {
+                                    checkLowerBound = false;
+                                    if (x >= Game.Current.BoardWidth)
+                                    {
+                                        isUpperBoundExceeded = true;
+                                        cellState = CellState.OutOfBounds;
+                                    }
+                                    else
+                                    {
+                                        cellState = walls[x, Centre.Y] ? CellState.Wall : CellState.Empty;
+                                    }
+                                }
+                            }
+                            if (includeOutOfBounds || cellState != CellState.OutOfBounds)
+                            {
+                                yield return new Cell { Pos = new Point( x, Centre.Y), State = cellState };
+                            }
+                        }
+                    }
+                    break;
+
+                case Core.Axis.Vertical:
+                    for (short y = (short) (Centre.Y - Constants.TANK_EXTENT_OFFSET); y < (short) (Centre.Y + Constants.TANK_EXTENT_OFFSET); y++)
+                    {
+                        if (checkLowerBound)
+                        {
+                            if (isUpperBoundExceeded)
+                            {
+                                cellState = CellState.OutOfBounds;
+                            }
+                            else
+                            {
+                                if (y < 0)
+                                {
+                                    cellState = CellState.OutOfBounds;
+                                }
+                                else
+                                {
+                                    checkLowerBound = false;
+                                    if (y >= Game.Current.BoardWidth)
+                                    {
+                                        isUpperBoundExceeded = true;
+                                        cellState = CellState.OutOfBounds;
+                                    }
+                                    else
+                                    {
+                                        cellState = walls[Centre.X, y] ? CellState.Wall : CellState.Empty;
+                                    }
+                                }
+                            }
+                            if (includeOutOfBounds || cellState != CellState.OutOfBounds)
+                            {
+                                yield return new Cell { Pos = new Point( Centre.X, y), State = cellState };
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Core/States/Cell.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AndrewTweddle.BattleCity.Core.States
+{
+    public struct Cell
+    {
+        public Point Pos { get; private set; }
+        public CellState State { get; private set; }
+
+        public Cell(Point pos, CellState state)
+        {
+            Pos = pos;
+            State = state;
+        }
+    }
+}
