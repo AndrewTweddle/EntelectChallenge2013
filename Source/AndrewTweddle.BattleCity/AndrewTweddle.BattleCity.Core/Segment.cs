2b35ff885ff2fc0eade95d8be38db030f4573e64
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2013-08-09 10:57:51 +0200

    Add Segment calculations for extended segments
    A segment might need to be extended one cell further in each direction
    e.g. for the outer edge of a tank. Or it might need to exclude the last
    cell, so that 4 edges of 4 cells each can be concatenated to form the
    boundary of a tank (or the points near a base where a tank can move to
    in order to destroy the base).

--- Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Core/Segment.cs
+++ Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Core/Segment.cs
@@ -98,5 +98,99 @@ namespace AndrewTweddle.BattleCity.Core
             }
             return cells;
         }
+
+        /// <summary>
+        /// It can also be useful to get an extended segment, for example an outside border of a tank, where each edge has 7 points, not 5.
+        /// Also by reducing the cell count by 1, the 4 edges of 4 points each (not 5) can be concatenated to form part of the border.
+        /// </summary>
+        /// <param name="walls"></param>
+        /// <param name="cellCount"></param>
+        /// <returns></returns>
+        public Cell[] GetCellsOnExtendedSegment(BitMatrix walls, int cellCount)
+        {
+            Cell[] cells = new Cell[cellCount];
+            byte i = 0;
+            CellState cellState;
+            bool checkLowerBound = true;
+            bool isUpperBoundExceeded = false;
+            short lowerBound;
+            short upperBound;
+
+            switch (Axis)
+            {
+                case Core.Axis.Horizontal:
+                    lowerBound = (short) (Centre.X - cellCount / 2);
+                    upperBound = (short) (Centre.X + (cellCount - 1) / 2);
+                    for (short x = lowerBound; x <= upperBound; x++, i++)
+                    {
+                        if (checkLowerBound)
+                        {
+                            if (isUpperBoundExceeded)
+                            {
+                                cellState = CellState.OutOfBounds;
+                            }
+                            else
+                            {
+                                if (x < 0)
+                                {
+                                    cellState = CellState.OutOfBounds;
+                                }
+                                else
+                                {
+                                    checkLowerBound = false;
+                                    if (x >= Game.Current.BoardWidth)
+                                    {
+                                        isUpperBoundExceeded = true;
+                                        cellState = CellState.OutOfBounds;
+                                    }
+                                    else
+                                    {
+                                        cellState = walls[x, Centre.Y] ? CellState.Wall : CellState.Empty;
+                                    }
+                                }
+                            }
+                            cells[i] = new Cell(pos: new Point(x, Centre.Y), state: cellState);
+                        }
+                    }
+                    break;
+
+                case Core.Axis.Vertical:
+                    lowerBound = (short) (Centre.Y - cellCount / 2);
+                    upperBound = (short) (Centre.Y + (cellCount - 1) / 2);
+                    for (short y = lowerBound; y <= upperBound; y++, i++)
+                    {
+                        if (checkLowerBound)
+                        {
+                            if (isUpperBoundExceeded)
+                            {
+                                cellState = CellState.OutOfBounds;
+                            }
+                            else
+                            {
+                                if (y < 0)
+                                {
+                                    cellState = CellState.OutOfBounds;
+                                }
+                                else
+                                {
+                                    checkLowerBound = false;
+                                    if (y >= Game.Current.BoardWidth)
+                                    {
+                                        isUpperBoundExceeded = true;
+                                        cellState = CellState.OutOfBounds;
+                                    }
+                                    else
+                                    {
+                                        cellState = walls[Centre.X, y] ? CellState.Wall : CellState.Empty;
+                                    }
+                                }
+                            }
+                            cells[i] = new Cell(pos: new Point(Centre.X, y), state: cellState);
+                        }
+                    }
+                    break;
+            }
+            return cells;
+        }
     }
 }
