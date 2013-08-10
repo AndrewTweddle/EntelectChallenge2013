5ed76a7e8bb50a128b0a1552a3cb0951fbe9d8ad
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2013-08-10 20:36:25 +0200

    Refactor and write performance stats to a file

--- Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Experimental.CommandLine/Program.cs
+++ Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Experimental.CommandLine/Program.cs
@@ -9,6 +9,8 @@ using AndrewTweddle.BattleCity.VisualUtils;
 using System.Drawing;
 using AndrewTweddle.BattleCity.Core;
 using System.Drawing.Imaging;
+using AndrewTweddle.BattleCity.Core.Calculations;
+using System.IO;
 
 namespace AndrewTweddle.BattleCity.Experimental.CommandLine
 {
@@ -18,6 +20,9 @@ namespace AndrewTweddle.BattleCity.Experimental.CommandLine
 
         static void Main(string[] args)
         {
+            string logFilePath = String.Format(
+                @"C:\Competitions\EntelectChallenge2013\temp\PerformanceStats\PerfStats_{0}.txt", DateTime.Now.ToString("yyyy-MM-dd_HHmmss"));
+
             // Test reading of JSon file:
             string jsonFilePath = @"C:\Competitions\EntelectChallenge2013\Harnesses\TestHarnessBeta\harness\boards\board.txt";
             BitMatrix board = JsonBoardReader.LoadBoardFromJsonFile(jsonFilePath);
@@ -36,18 +41,20 @@ namespace AndrewTweddle.BattleCity.Experimental.CommandLine
 
             // Test segment type calculations:
             title = String.Format("Test calculation of vertical segment state matrix using a {0}", bitMatrixType);
-            TimeActionWithArgument(title, repetitions, board, GetVerticalSegmentStateMatrix);
+            Matrix<SegmentState> vertSegStateMatrix 
+                = TimeFunctionWithArgument(logFilePath, title, repetitions, board, GetVerticalSegmentStateMatrix);
 
-            Matrix<SegmentState> vertSegStateMatrix = board.GetBoardSegmentMatrixForAxisOfMovement(Axis.Vertical);
+            // Save image for vertical segment state matrix:
             Bitmap segStateBitmap = imageGen.GenerateBoardImage(board);
             imageGen.DrawSegmentMatrixOverlay(segStateBitmap, board, vertSegStateMatrix, Axis.Vertical);
             string segmentMatrixFilePath = @"c:\Competitions\EntelectChallenge2013\temp\VertSegmentMatrix.bmp";
             segStateBitmap.Save(segmentMatrixFilePath, ImageFormat.Bmp);
 
             title = String.Format("Test calculation of horizontal segment state matrix using a {0}", bitMatrixType);
-            TimeActionWithArgument(title, repetitions, board, GetHorizontalSegmentStateMatrix);
+            Matrix<SegmentState> horizSegStateMatrix 
+                = TimeFunctionWithArgument(logFilePath, title, repetitions, board, GetHorizontalSegmentStateMatrix);
 
-            Matrix<SegmentState> horizSegStateMatrix = board.GetBoardSegmentMatrixForAxisOfMovement(Axis.Horizontal);
+            // Save image for horizontal segment state matrix:
             segStateBitmap = imageGen.GenerateBoardImage(board);
             imageGen.DrawSegmentMatrixOverlay(segStateBitmap, board, horizSegStateMatrix, Axis.Horizontal);
             segmentMatrixFilePath = @"c:\Competitions\EntelectChallenge2013\temp\HorizSegmentMatrix.bmp";
@@ -57,76 +64,48 @@ namespace AndrewTweddle.BattleCity.Experimental.CommandLine
             segmentMatrixFilePath = @"c:\Competitions\EntelectChallenge2013\temp\BiDiSegmentMatrix.bmp";
             segStateBitmap.Save(segmentMatrixFilePath, ImageFormat.Bmp);
 
+            // Test calculation caches:
+            TimeActionWithArgument(logFilePath, "Time cell calculator on challenge board 1", 10, board, PerformCellCalculation);
+
             // Test construction time for a BitMatrix:
             title = String.Format("Construct and populate a {0}", bitMatrixType);
-            TimeAction(title, repetitions, ConstructBitMatrix);
+            BitMatrix bm = TimeFunction(logFilePath, title, repetitions, ConstructBitMatrix);
 
-            BitMatrix bm = new BitMatrix(BIT_MATRIX_DIMENSION, BIT_MATRIX_DIMENSION);
-            for (short x = 0; x < BIT_MATRIX_DIMENSION; x++)
-            {
-                for (short y = 0; y < BIT_MATRIX_DIMENSION; y++)
-                {
-                    bm[x, y] = x + y % 2 == 1;
-                }
-            }
             title = String.Format("Read a {0}", bitMatrixType);
-            TimeActionWithArgument(title, repetitions, bm, ReadBitMatrix);
+            TimeActionWithArgument(logFilePath, title, repetitions, bm, ReadBitMatrix);
 
             title = String.Format("Clone a {0}", bitMatrixType);
-            TimeActionWithArgument(title, repetitions, bm, CloneBitMatrix);
+            TimeActionWithArgument(logFilePath, title, repetitions, bm, CloneBitMatrix);
         }
 
-        private static void GetVerticalSegmentStateMatrix(BitMatrix board)
+        private static void PerformCellCalculation(BitMatrix board)
+        {
+            Matrix<CellCalculation> matrix = CellCalculator.Calculate(board);
+        }
+
+        private static Matrix<SegmentState> GetVerticalSegmentStateMatrix(BitMatrix board)
         {
             Matrix<SegmentState> segStateMatrix = board.GetBoardSegmentMatrixForAxisOfMovement(Axis.Vertical);
+            return segStateMatrix;
         }
 
-        private static void GetHorizontalSegmentStateMatrix(BitMatrix board)
+        private static Matrix<SegmentState> GetHorizontalSegmentStateMatrix(BitMatrix board)
         {
             Matrix<SegmentState> segStateMatrix = board.GetBoardSegmentMatrixForAxisOfMovement(Axis.Horizontal);
+            return segStateMatrix;
         }
 
-        private static void TimeAction(string title, int repetitions, Action action)
-        {
-            Console.WriteLine("Timing action: {0}", title);
-            Stopwatch sw = Stopwatch.StartNew();
-            for (int i = 0; i < repetitions; i++)
-            {
-                action();
-            }
-            sw.Stop();
-            Console.WriteLine("Duration for {0} repetitions: {1}", repetitions, sw.Elapsed);
-            Console.WriteLine("Average duration: {0} microseconds", sw.ElapsedMilliseconds * 1000.0 / repetitions);
-            Console.WriteLine("-------------------------------------------------------------------------------");
-            Console.WriteLine();
-        }
-
-        private static void TimeActionWithArgument<T>(string title, int repetitions, T arg, Action<T> action)
-        {
-            Console.WriteLine("Timing action: {0}", title);
-            Console.WriteLine("Starting at: {0}", DateTime.Now);
-            Stopwatch sw = Stopwatch.StartNew();
-            for (int i = 0; i < repetitions; i++)
-            {
-                action(arg);
-            }
-            sw.Stop();
-            Console.WriteLine("Duration for {0} repetitions: {1}", repetitions, sw.Elapsed);
-            Console.WriteLine("Average duration: {0} microseconds", sw.ElapsedMilliseconds * 1000.0 / repetitions);
-            Console.WriteLine("-------------------------------------------------------------------------------");
-            Console.WriteLine();
-        }
-
-        private static void ConstructBitMatrix()
+        private static BitMatrix ConstructBitMatrix()
         {
             BitMatrix bm = new BitMatrix(BIT_MATRIX_DIMENSION, BIT_MATRIX_DIMENSION);
-            for (short x = 0; x < BIT_MATRIX_DIMENSION; x++)
+            for (int x = 0; x < BIT_MATRIX_DIMENSION; x++)
             {
-                for (short y = 0; y < BIT_MATRIX_DIMENSION; y++)
+                for (int y = 0; y < BIT_MATRIX_DIMENSION; y++)
                 {
                     bm[x, y] = x + y % 2 == 1;
                 }
             }
+            return bm;
         }
 
         private static void ReadBitMatrix(BitMatrix matrixToRead)
@@ -206,5 +185,85 @@ namespace AndrewTweddle.BattleCity.Experimental.CommandLine
         
         */
 
+        #region Utility methods for timing and reporting on actions
+
+        private static void TimeAction(string logFilePath, string title, int repetitions, Action action)
+        {
+            WriteTestTitle(logFilePath, title);
+
+            Stopwatch swatch = Stopwatch.StartNew();
+            for (int i = 0; i < repetitions; i++)
+            {
+                action();
+            }
+            swatch.Stop();
+
+            WriteDurationStats(logFilePath, repetitions, swatch);
+        }
+
+        private static void TimeActionWithArgument<T>(string logFilePath, string title, int repetitions, T arg, Action<T> action)
+        {
+            WriteTestTitle(logFilePath, title);
+            Stopwatch swatch = Stopwatch.StartNew();
+            for (int i = 0; i < repetitions; i++)
+            {
+                action(arg);
+            }
+            swatch.Stop();
+            WriteDurationStats(logFilePath, repetitions, swatch);
+        }
+
+        private static TResult TimeFunction<TResult>(string logFilePath, string title, int repetitions,
+            Func<TResult> function)
+        {
+            TResult result = default(TResult);
+            WriteTestTitle(logFilePath, title);
+            Stopwatch swatch = Stopwatch.StartNew();
+            for (int i = 0; i < repetitions; i++)
+            {
+                result = function();
+            }
+            swatch.Stop();
+            WriteDurationStats(logFilePath, repetitions, swatch);
+            return result;
+        }
+
+        private static TResult TimeFunctionWithArgument<T, TResult>(string logFilePath, string title, int repetitions,
+            T arg, Func<T, TResult> function)
+        {
+            TResult result = default(TResult);
+            WriteTestTitle(logFilePath, title);
+            Stopwatch swatch = Stopwatch.StartNew();
+            for (int i = 0; i < repetitions; i++)
+            {
+                result = function(arg);
+            }
+            swatch.Stop();
+            WriteDurationStats(logFilePath, repetitions, swatch);
+            return result;
+        }
+
+        private static void WriteTestTitle(string logFilePath, string title)
+        {
+            string titleText = String.Format("Timing action: {0}\r\n\r\nStarting at: {1}", title, DateTime.Now);
+            File.AppendAllText(logFilePath, titleText);
+            Console.WriteLine(titleText);
+        }
+
+        private static void WriteDurationStats(string logFilePath, int repetitions, Stopwatch swatch)
+        {
+            StringBuilder sb = new StringBuilder();
+            StringWriter sw = new StringWriter(sb);
+            sw.WriteLine("Duration for {0} repetitions: {1}", repetitions, swatch.Elapsed);
+            sw.WriteLine("Average duration: {0} microseconds", swatch.ElapsedMilliseconds * 1000.0 / repetitions);
+            sw.WriteLine("-------------------------------------------------------------------------------");
+            sw.WriteLine();
+            sw.Flush();
+            string durationText = sb.ToString();
+            Console.Write(durationText);
+            File.AppendAllText(logFilePath, durationText);
+        }
+        
+        #endregion
     }
 }
