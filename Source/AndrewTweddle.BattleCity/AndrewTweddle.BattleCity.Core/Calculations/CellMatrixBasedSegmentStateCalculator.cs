76b72a5ce5351d774f687e65516d34fee87e3443
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2013-08-17 16:50:13 +0200

    Use cell matrix to do segment state calculations
    Compared performance using cell matrix to assist with segment state
    calculations. This performed similarly to direct calculations in one
    case, and worst of all in the other (random points) case.

---
+++ Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Core/Calculations/CellMatrixBasedSegmentStateCalculator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndrewTweddle.BattleCity.Core.Collections;
+
+namespace AndrewTweddle.BattleCity.Core.Calculations
+{
+    public class CellMatrixBasedSegmentStateCalculator: ISegmentStateCalculator
+    {
+        BitMatrix Board { get; set; }
+        Matrix<Cell> CellMatrix { get; set; }
+
+        public CellMatrixBasedSegmentStateCalculator(BitMatrix board, Matrix<Cell> cellMatrix)
+        {
+            Board = board;
+            CellMatrix = cellMatrix;
+        }
+
+        public SegmentState GetSegmentState(Axis axisOfMovement, Point point)
+        {
+            Segment segment = CellMatrix[point].GetSegmentByAxisOfMovement(axisOfMovement);
+            if (segment.IsOutOfBounds)
+            {
+                return SegmentState.OutOfBounds;
+            }
+            else
+            {
+                foreach (BitMatrixMask mask in segment.BitMasksOfPoints)
+                {
+                    if (Board[mask])
+                    {
+                        if (Board[segment.CentreCell.BitIndexAndMask])
+                        {
+                            return SegmentState.ShootableWall;
+                        }
+                        else
+                        {
+                            return SegmentState.UnshootablePartialWall;
+                        }
+                    }
+                }
+                return SegmentState.Clear;
+            }
+        }
+
+        public SegmentState GetSegmentState(Axis axisOfMovement, int x, int y)
+        {
+            Segment segment = CellMatrix[x, y].GetSegmentByAxisOfMovement(axisOfMovement);
+            if (segment.IsOutOfBounds)
+            {
+                return SegmentState.OutOfBounds;
+            }
+            else
+            {
+                foreach (BitMatrixMask mask in segment.BitMasksOfPoints)
+                {
+                    if (Board[mask])
+                    {
+                        if (Board[segment.CentreCell.BitIndexAndMask])
+                        {
+                            return SegmentState.ShootableWall;
+                        }
+                        else
+                        {
+                            return SegmentState.UnshootablePartialWall;
+                        }
+                    }
+                }
+                return SegmentState.Clear;
+            }
+        }
+    }
+}
--- Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Experimental.CommandLine/Program.cs
+++ Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Experimental.CommandLine/Program.cs
@@ -142,15 +142,17 @@ namespace AndrewTweddle.BattleCity.Experimental.CommandLine
                 repetitions, board, b => b.ConvertToBoolMatrix());
 
             /* Compare segment state calculation times over the whole board for cached and calculated segment state calculators: */
-            ComparePerformanceOfCachedAndOnTheFlySegmentStateCalculators(logFilePath, repetitions, 
-                board, vertSegStateMatrix, horizSegStateMatrix, boolMatrix);
+            ComparePerformanceOfCachedAndOnTheFlySegmentStateCalculators(logFilePath, repetitions,
+                board, cellMatrix, vertSegStateMatrix, horizSegStateMatrix, boolMatrix);
 
             // Test construction time for a BitMatrix:
             TestBitMatrix(logFilePath, repetitions);
         }
 
+        #region Segment state calculations
+
         private static void ComparePerformanceOfCachedAndOnTheFlySegmentStateCalculators(
-            string logFilePath, int repetitions, BitMatrix board, 
+            string logFilePath, int repetitions, BitMatrix board, Matrix<Cell> cellMatrix,
             Matrix<SegmentState> vertSegStateMatrix, Matrix<SegmentState> horizSegStateMatrix, Matrix<bool> boolMatrix)
         {
             // Cache based segment state calculator:
@@ -172,6 +174,12 @@ namespace AndrewTweddle.BattleCity.Experimental.CommandLine
                 Tuple.Create(boolMatrix.BottomRight, boolMatrixBasedSegStateCalculator),
                 CalculateSegmentStatesUsingOnTheFlyBoolBoolBasedCalculator);
 
+            CellMatrixBasedSegmentStateCalculator cellCacheSegStateCalculator 
+                = new CellMatrixBasedSegmentStateCalculator(board, cellMatrix);
+            TimeActionWithArgument(logFilePath, "Calculate all segments using cell matrix-based cache calculator",
+                repetitions, Tuple.Create(board.BottomRight, cellCacheSegStateCalculator),
+                CalculateSegmentStatesUsingCellCacheBasedCalculator);
+
             /* Compare segment state calculation times over random points for cached and calculated segment state calculators, 
              * to make sure the cache calculator is not just benefiting from memory cache effects due to the sequence of points: 
              */
@@ -180,6 +188,10 @@ namespace AndrewTweddle.BattleCity.Experimental.CommandLine
             // Cache based segment state calculator:
             TimeActionWithArgument(logFilePath, "Calculate segments on random points using cache calculator", repetitions,
                 Tuple.Create(randomPoints, cacheSegStateCalculator),
+                CalculateSegmentStatesOnRandomPointsUsingCache);
+
+            TimeActionWithArgument(logFilePath, "Calculate segments on random points using cell matrix-based cache calculator", repetitions,
+                Tuple.Create(randomPoints, cellCacheSegStateCalculator),
                 CalculateSegmentStatesOnRandomPointsUsingCache);
 
             // Calculation based segment state calculator:
@@ -192,6 +204,128 @@ namespace AndrewTweddle.BattleCity.Experimental.CommandLine
                 repetitions, Tuple.Create(randomPoints, boolMatrixBasedSegStateCalculator),
                 CalculateSegmentStatesOnRandomPointsUsingOnTheFlyBoolBoolBasedCalculator);
         }
+
+        private static void CalculateSegmentStatesUsingCache(Tuple<Core.Point, CacheBasedSegmentStateCalculator> tuple)
+        {
+            AndrewTweddle.BattleCity.Core.Point bottomRight = tuple.Item1;
+            CacheBasedSegmentStateCalculator calculator = tuple.Item2;
+            foreach (Axis axis in BoardHelper.AllRealAxes)
+            {
+                for (int x = 0; x < bottomRight.X; x++)
+                {
+                    for (int y = 0; y < bottomRight.Y; y++)
+                    {
+                        SegmentState segState = calculator.GetSegmentState(axis, x, y);
+                    }
+                }
+            }
+        }
+
+        private static void CalculateSegmentStatesUsingCellCacheBasedCalculator(Tuple<Core.Point, CellMatrixBasedSegmentStateCalculator> tuple)
+        {
+            AndrewTweddle.BattleCity.Core.Point bottomRight = tuple.Item1;
+            CellMatrixBasedSegmentStateCalculator calculator = tuple.Item2;
+            foreach (Axis axis in BoardHelper.AllRealAxes)
+            {
+                for (int x = 0; x < bottomRight.X; x++)
+                {
+                    for (int y = 0; y < bottomRight.Y; y++)
+                    {
+                        SegmentState segState = calculator.GetSegmentState(axis, x, y);
+                    }
+                }
+            }
+        }
+
+        public static void CalculateSegmentStatesUsingCalculationBasedCalculator(
+            Tuple<Core.Point, CalculationBasedSegmentStateCalculator> tuple)
+        {
+            Core.Point bottomRight = tuple.Item1;
+            CalculationBasedSegmentStateCalculator calculator = tuple.Item2;
+            foreach (Axis axis in BoardHelper.AllRealAxes)
+            {
+                for (int x = 0; x < bottomRight.X; x++)
+                {
+                    for (int y = 0; y < bottomRight.Y; y++)
+                    {
+                        SegmentState segState = calculator.GetSegmentState(axis, x, y);
+                    }
+                }
+            }
+        }
+
+        public static void CalculateSegmentStatesUsingOnTheFlyBoolBoolBasedCalculator(
+            Tuple<Core.Point, OnTheFlyBoolMatrixBasedSegmentStateCalculator> tuple)
+        {
+            Core.Point bottomRight = tuple.Item1;
+            OnTheFlyBoolMatrixBasedSegmentStateCalculator calculator = tuple.Item2;
+            foreach (Axis axis in BoardHelper.AllRealAxes)
+            {
+                for (int x = 0; x < bottomRight.X; x++)
+                {
+                    for (int y = 0; y < bottomRight.Y; y++)
+                    {
+                        SegmentState segState = calculator.GetSegmentState(axis, x, y);
+                    }
+                }
+            }
+        }
+
+        public static void CalculateSegmentStatesOnRandomPointsUsingCache(Tuple<Core.Point[], CacheBasedSegmentStateCalculator> tuple)
+        {
+            Core.Point[] randomPoints = tuple.Item1;
+            CacheBasedSegmentStateCalculator calculator = tuple.Item2;
+            foreach (Axis axis in BoardHelper.AllRealAxes)
+            {
+                foreach (Core.Point randomPoint in randomPoints)
+                {
+                    SegmentState segState = calculator.GetSegmentState(axis, randomPoint);
+                }
+            }
+        }
+
+        public static void CalculateSegmentStatesOnRandomPointsUsingCache(Tuple<Core.Point[], CellMatrixBasedSegmentStateCalculator> tuple)
+        {
+            Core.Point[] randomPoints = tuple.Item1;
+            CellMatrixBasedSegmentStateCalculator calculator = tuple.Item2;
+            foreach (Axis axis in BoardHelper.AllRealAxes)
+            {
+                foreach (Core.Point randomPoint in randomPoints)
+                {
+                    SegmentState segState = calculator.GetSegmentState(axis, randomPoint);
+                }
+            }
+        }
+
+        public static void CalculateSegmentStatesOnRandomPointsUsingCalculationBasedCalculator(
+            Tuple<Core.Point[], CalculationBasedSegmentStateCalculator> tuple)
+        {
+            Core.Point[] randomPoints = tuple.Item1;
+            CalculationBasedSegmentStateCalculator calculator = tuple.Item2;
+            foreach (Axis axis in BoardHelper.AllRealAxes)
+            {
+                foreach (Core.Point randomPoint in randomPoints)
+                {
+                    SegmentState segState = calculator.GetSegmentState(axis, randomPoint);
+                }
+            }
+        }
+
+        public static void CalculateSegmentStatesOnRandomPointsUsingOnTheFlyBoolBoolBasedCalculator(
+            Tuple<Core.Point[], OnTheFlyBoolMatrixBasedSegmentStateCalculator> tuple)
+        {
+            Core.Point[] randomPoints = tuple.Item1;
+            OnTheFlyBoolMatrixBasedSegmentStateCalculator calculator = tuple.Item2;
+            foreach (Axis axis in BoardHelper.AllRealAxes)
+            {
+                foreach (Core.Point randomPoint in randomPoints)
+                {
+                    SegmentState segState = calculator.GetSegmentState(axis, randomPoint);
+                }
+            }
+        }
+
+        #endregion
 
         private static void TestBitMatrix(string logFilePath, int repetitions)
         {
@@ -294,7 +428,8 @@ namespace AndrewTweddle.BattleCity.Experimental.CommandLine
             }
         }
 
-        private static void TimeActionWithArgument<T>(string logFilePath, string title, int repetitions, T arg, Action<T> action)
+        private static void TimeActionWithArgument<T>(string logFilePath, 
+            string title, int repetitions, T arg, Action<T> action)
         {
             WriteTestTitle(logFilePath, title);
             Stopwatch swatch = Stopwatch.StartNew();
@@ -405,97 +540,6 @@ namespace AndrewTweddle.BattleCity.Experimental.CommandLine
         }
         
         #endregion
-
-        public static void CalculateSegmentStatesUsingCache(Tuple<Core.Point, CacheBasedSegmentStateCalculator> tuple)
-        {
-            AndrewTweddle.BattleCity.Core.Point bottomRight = tuple.Item1;
-            CacheBasedSegmentStateCalculator calculator = tuple.Item2;
-            foreach (Axis axis in BoardHelper.AllRealAxes)
-            {
-                for (int x = 0; x < bottomRight.X; x++)
-                {
-                    for (int y = 0; y < bottomRight.Y; y++)
-                    {
-                        SegmentState segState = calculator.GetSegmentState(axis, x, y);
-                    }
-                }
-            }
-        }
-
-        public static void CalculateSegmentStatesUsingCalculationBasedCalculator(
-            Tuple<Core.Point, CalculationBasedSegmentStateCalculator> tuple)
-        {
-            Core.Point bottomRight = tuple.Item1;
-            CalculationBasedSegmentStateCalculator calculator = tuple.Item2;
-            foreach (Axis axis in BoardHelper.AllRealAxes)
-            {
-                for (int x = 0; x < bottomRight.X; x++)
-                {
-                    for (int y = 0; y < bottomRight.Y; y++)
-                    {
-                        SegmentState segState = calculator.GetSegmentState(axis, x, y);
-                    }
-                }
-            }
-        }
-
-        public static void CalculateSegmentStatesUsingOnTheFlyBoolBoolBasedCalculator(
-            Tuple<Core.Point, OnTheFlyBoolMatrixBasedSegmentStateCalculator> tuple)
-        {
-            Core.Point bottomRight = tuple.Item1;
-            OnTheFlyBoolMatrixBasedSegmentStateCalculator calculator = tuple.Item2;
-            foreach (Axis axis in BoardHelper.AllRealAxes)
-            {
-                for (int x = 0; x < bottomRight.X; x++)
-                {
-                    for (int y = 0; y < bottomRight.Y; y++)
-                    {
-                        SegmentState segState = calculator.GetSegmentState(axis, x, y);
-                    }
-                }
-            }
-        }
-
-        public static void CalculateSegmentStatesOnRandomPointsUsingCache(Tuple<Core.Point[], CacheBasedSegmentStateCalculator> tuple)
-        {
-            Core.Point[] randomPoints = tuple.Item1;
-            CacheBasedSegmentStateCalculator calculator = tuple.Item2;
-            foreach (Axis axis in BoardHelper.AllRealAxes)
-            {
-                foreach (Core.Point randomPoint in randomPoints)
-                {
-                    SegmentState segState = calculator.GetSegmentState(axis, randomPoint);
-                }
-            }
-        }
-
-        public static void CalculateSegmentStatesOnRandomPointsUsingCalculationBasedCalculator(
-            Tuple<Core.Point[], CalculationBasedSegmentStateCalculator> tuple)
-        {
-            Core.Point[] randomPoints = tuple.Item1;
-            CalculationBasedSegmentStateCalculator calculator = tuple.Item2;
-            foreach (Axis axis in BoardHelper.AllRealAxes)
-            {
-                foreach (Core.Point randomPoint in randomPoints)
-                {
-                    SegmentState segState = calculator.GetSegmentState(axis, randomPoint);
-                }
-            }
-        }
-
-        public static void CalculateSegmentStatesOnRandomPointsUsingOnTheFlyBoolBoolBasedCalculator(
-            Tuple<Core.Point[], OnTheFlyBoolMatrixBasedSegmentStateCalculator> tuple)
-        {
-            Core.Point[] randomPoints = tuple.Item1;
-            OnTheFlyBoolMatrixBasedSegmentStateCalculator calculator = tuple.Item2;
-            foreach (Axis axis in BoardHelper.AllRealAxes)
-            {
-                foreach (Core.Point randomPoint in randomPoints)
-                {
-                    SegmentState segState = calculator.GetSegmentState(axis, randomPoint);
-                }
-            }
-        }
 
         /// <summary>
         /// 
