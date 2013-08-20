d197c1c02da0fe365909a8172c0f6b9e6587cc4d
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2013-08-20 22:36:27 +0200

    Experimental code to test challenge harness wsdls
    svcutil caused the Entelect test harness to crash. Check if adding
    service references via Visual Studio works.
    
    Result: Yes it does. Just increase maxReceivedMessageSize on the binding
    due to the number of cells returned by the login call.

---
+++ Experiments/WebServiceClientTestApp/WebServiceClientTestApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebServiceClientTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChallengeService.ChallengeClient client = new ChallengeService.ChallengeClient();
+            client.Open();
+            try
+            {
+                var states = client.login();
+                int c = 0;
+                foreach (var col in states)
+                {
+                    int r = 0;
+                    Console.WriteLine("column {0}: ", c);
+                    foreach (var row in col)
+                    {
+                        Console.WriteLine("   row {0}: {1}", r, row);
+                        r++;
+                    }
+                    c++;
+                }
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
---
+++ Experiments/WebServiceClientTestApp/WebServiceClientTestApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebServiceClientTestApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ChallengeService2.ChallengeClient();
+            client.Open();
+            try
+            {
+                var states = client.login();
+                int c = 0;
+                foreach (var col in states)
+                {
+                    int r = 0;
+                    Console.WriteLine("column {0}: ", c);
+                    foreach (var row in col)
+                    {
+                        Console.WriteLine("   row {0}: {1}", r, row);
+                        r++;
+                    }
+                    c++;
+                }
+            }
+            finally
+            {
+                client.Close();
+            }
+        }
+    }
+}
