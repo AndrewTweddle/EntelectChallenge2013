b9f75a3978b030d3f63d56addf810713785bee9f
Andrew Tweddle <AndrewTweddle@fakeEMail.com>
2013-08-30 16:57:30 +0200

    Updated shortest path bot to use attack distances

--- Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Bots/ShortestPathBot.cs
+++ Source/AndrewTweddle.BattleCity/AndrewTweddle.BattleCity.Bots/ShortestPathBot.cs
@@ -11,6 +11,7 @@ using AndrewTweddle.BattleCity.Core.Collections;
 using AndrewTweddle.BattleCity.Core.Calculations.Distances;
 using AndrewTweddle.BattleCity.Core.Helpers;
 using AndrewTweddle.BattleCity.Core.Calculations;
+using AndrewTweddle.BattleCity.Core.Calculations.Firing;
 
 namespace AndrewTweddle.BattleCity.Bots
 {
@@ -28,77 +29,54 @@ namespace AndrewTweddle.BattleCity.Bots
         protected override void ChooseMoves()
         {
             GameState currGameState = Game.Current.CurrentTurn.GameState;
-            TankActionSet actionSet = new TankActionSet(YourPlayerIndex, currGameState.Tick);
 
-            Base @base = Opponent.Base;
+            Tuple<int, int, TankAction>[] tankNumberDistanceAndActionArray = new Tuple<int, int, TankAction>[Constants.TANKS_PER_PLAYER];
+
             for (int tankNumber = 0; tankNumber < Constants.TANKS_PER_PLAYER; tankNumber++)
             {
                 Tank tank = You.Tanks[tankNumber];
                 MobileState tankState = currGameState.GetMobileState(tank.Index);
                 if (!tankState.IsActive)
                 {
+                    tankNumberDistanceAndActionArray[tankNumber] 
+                        = new Tuple<int, int, TankAction>(tankNumber, Constants.UNREACHABLE_DISTANCE, TankAction.NONE);
                     continue;
                 }
 
-                DirectionalMatrix<DistanceCalculation> distances = currGameState.CalculationCache.GetDistanceMatrixForTank(tank.Index);
+                Base enemyBase = Opponent.Base;
+                DirectionalMatrix<DistanceCalculation> distancesToEnemyBase
+                    = currGameState.CalculationCache.GetIncomingDistanceMatrixForBase(Opponent.Index);
+                DistanceCalculation distanceCalc = distancesToEnemyBase[tankState.Dir, tankState.Pos];
 
-                List<Path> shortestPaths = new List<Path>();
-                int shortestDistance = Constants.UNREACHABLE_DISTANCE;
+                FiringLineMatrix firingLineMatrix = currGameState.CalculationCache.FiringLinesForPointsMatrix;
+                TankAction[] tankActions = PathCalculator.GetTankActionsOnIncomingShortestPath(distancesToEnemyBase,
+                    tankState.Dir, tankState.Pos.X, tankState.Pos.Y, enemyBase.Pos.X, enemyBase.Pos.Y, 
+                    keepMovingCloserOnFiringLastBullet: false);
 
-                TankLocation tankLoc = Game.Current.CurrentTurn.CalculationCache.TankLocationMatrix[@base.Pos];
-
-                // Get the distances to move onto the base from various directions:
-                foreach (Direction dir in BoardHelper.AllRealDirections)
+                if (tankActions.Length == 0)
                 {
-                    Direction oppositeDir = dir.GetOpposite();
-
-                    // Get the inside edge (segment) of the tank centred at the base in the opposite direction:
-                    Segment tankPositionsInDirection = tankLoc.InsideEdgesByDirection[(int)oppositeDir];
-
-                    // For each point on the segment determine the moving distance to that point:
-                    foreach (Cell tankCellInDir in tankPositionsInDirection.Cells)
-                    {
-                        if (tankCellInDir.IsValid)
-                        {
-                            DistanceCalculation distCalc = distances[dir, tankCellInDir.Position];
-                            if (distCalc.CodedDistance != 0)
-                            {
-                                if (distCalc.Distance <= shortestDistance)
-                                {
-                                    if (distCalc.Distance != shortestDistance)
-                                    {
-                                        shortestPaths.Clear();
-                                    }
-                                    Path path = new Path
-                                    {
-                                        FinalDirection = dir,
-                                        FinalPosition = tankCellInDir.Position,
-                                        DistanceCalculation = distCalc
-                                    };
-                                    shortestPaths.Add(path);
-                                }
-                            }
-                        }
-                    }
+                    tankNumberDistanceAndActionArray[tankNumber]
+                        = new Tuple<int, int, TankAction>(tankNumber, Constants.UNREACHABLE_DISTANCE, TankAction.NONE);
                 }
-
-                // TODO: Get the distances to move into a firing position plus the distance to shoot at the base
-                // TODO: Work out how to avoid multiple identical paths 
-                // - perhaps only consider paths which don't approach along the axis of firing
-
-                if (shortestPaths.Count > 0)
+                else
                 {
-                    Random rnd = new Random();
-                    int shortestPathIndex = rnd.Next(shortestPaths.Count);
-                    Path shortestPathChosen = shortestPaths[shortestPathIndex];
-                    TankAction[] pointsOnPath = DistanceCalculator.GetTankActionsOnShortestPath(
-                        distances, shortestPathChosen.FinalDirection, shortestPathChosen.FinalPosition);
-                    if (pointsOnPath.Length > 0)
-                    {
-                        actionSet.Actions[tankNumber] = pointsOnPath[0];
-                    }
+                    tankNumberDistanceAndActionArray[tankNumber]
+                        = new Tuple<int, int, TankAction>(tankNumber, distanceCalc.Distance, tankActions[0]);
                 }
             }
+
+            int chosenTank;
+            if (tankNumberDistanceAndActionArray[1].Item2 < tankNumberDistanceAndActionArray[0].Item2)
+            {
+                chosenTank = 1;
+            }
+            else
+            {
+                chosenTank = 0;
+            }
+            TankActionSet actionSet = new TankActionSet(YourPlayerIndex, currGameState.Tick);
+            actionSet.Actions[chosenTank] = tankNumberDistanceAndActionArray[chosenTank].Item3;
+            actionSet.Actions[1 - chosenTank] = TankAction.NONE;
 
             Coordinator.SetBestMoveSoFar(actionSet);
         }
